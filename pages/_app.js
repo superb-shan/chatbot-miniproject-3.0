7eb2ae0f8cbad44e7ea00f1d0d5d9f0834760667
superb-shan <shankar.k2021csec@sece.ac.in>
2023-04-28 20:31:40 +0530

    Title change

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>LearnMate Chat</title>
         <link rel="stylesheet" href="../styles/global.css" />
       </Head>
       <ContextProvider>
